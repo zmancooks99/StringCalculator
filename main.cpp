08bf1a9eb6817c2295dac11e360b0a798f190735
zmancooks99 <zmancooks99@yahoo.com>
2020-02-05 16:10:54 -0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StringCalculator
+//
+//  Created by Zachary Mason on 2/5/20.
+//  Copyright © 2020 Zachary Mason. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
